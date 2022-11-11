a3e3f8f71f23a53214a0e3152ee458fb99b741fd
Kenneth Heng <kennethhengfucheng@gmail.com>
2022-11-11 17:05:50 +0800

    feat: sync fron-end to back-end

---
+++ src/routes/aid.ts
@@ -0,0 +1,20 @@
+import express from "express";
+
+const router = express.Router();
+
+router.use(express.json());
+
+router.get("/aid", async (req, res) => {
+	res.send("All Aid Get Route");
+});
+router.post("/aid", async (req, res) => {
+	res.send("All Aid Post Route");
+});
+router.get("/aid/:aid_id", async (req, res) => {
+	res.send("Aid Get Route");
+});
+router.post("/aid/:aid_id", async (req, res) => {
+	res.send("Aid Post Route");
+});
+
+export default router;
